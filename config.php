830f25d392d0c6d2d44644bd0fa29030a6d309f8
Debarati Pramanik <pramanikdebarati94@gmail.com>
2026-06-05 18:35:18 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,11 +2,11 @@
 // Configuration File for Hosting (PostgreSQL)
 // Replace these values with your live server database credentials
 
-define('dpg-d8hc255dt1ts7389ic90-a', 'localhost'); // Your PostgreSQL host (e.g., dpg-xxxxx.render.com)
-define('5432', '5432');      // PostgreSQL default port is 5432
-define('blog_user', 'postgres');  // Your database username
-define('uCITM3bW7mOlVyZQLlwLhCF52qSF9WGs', '');          // Your database password
-define('blog_db_wcgu', 'blog_db');   // Your database name
+define('DB_HOST', 'dpg-d8hc255dt1ts7389ic90-a.ohio-postgres.render.com'); 
+define('DB_PORT', '5432');      
+define('DB_USER', 'blog_user');  
+define('DB_PASS', 'uCITM3bW7mOlVyZQLlwLhCF52qSF9WGs');          
+define('DB_NAME', 'blog_db_wcgu');   
 
 // Optional: Base URL of your website (e.g., https://yourdomain.com/)
 define('BASE_URL', 'http://localhost/XYZ_Blogs/');
